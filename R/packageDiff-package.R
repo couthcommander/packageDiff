57c6f55ba5821a5e0c0f053534ee2c35fb9c9492
beckca <cole.beck@vanderbilt.edu>
2019-07-09 15:04:20 -0500

    fix namespace

---
+++ R/packageDiff-package.R
@@ -0,0 +1,13 @@
+#' Package Diff
+#'
+#' Extract package information and compare between versions.
+#'
+#' @docType package
+#'
+#' @author Cole Beck \email{cole.beck@@vumc.org}
+#'
+#' Maintainer: Cole Beck \email{cole.beck@@vumc.org}
+#'
+#' @importFrom methods formalArgs
+#' @importFrom utils capture.output data download.file packageDescription untar
+"_PACKAGE"
